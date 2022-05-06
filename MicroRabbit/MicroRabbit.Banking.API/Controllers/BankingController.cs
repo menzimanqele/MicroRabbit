c848b43ae95eb7d9239e31c52747a80761d47254
Menzi Manqele <manqele@live.com>
2022-05-06 12:47:08 +0200

    Implemented Publish & SendCommand for Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.API/Controllers/BankingController.cs
+++ MicroRabbit/MicroRabbit.Banking.API/Controllers/BankingController.cs
@@ -1,4 +1,5 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,13 @@ namespace MicroRabbit.Banking.API.Controllers
         public ActionResult <IEnumerable<Account>> Get()
         {
             return Ok(accountService.GetAccounts());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            this.accountService.Transfer(accountTransfer);
+            return Ok(accountTransfer);
         }
     }
 }
--- MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,14 +13,25 @@ namespace MicroRabbit.Banking.Application.Services
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository accountRepository;
+        private readonly IEventBus _bus;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
             this.accountRepository = accountRepository;
+            this._bus = bus;    
         }
         public IEnumerable<Account> GetAccounts()
         {
             return accountRepository.GetAccount();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                accountTransfer.FromAccount,accountTransfer.ToAccount,accountTransfer.TransferAmount
+                );
+            _bus.sendCommand(createTransferCommand);
+
         }
     }
 }
