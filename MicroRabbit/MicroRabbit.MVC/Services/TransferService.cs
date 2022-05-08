0d74990de7032b757910c740fb5c2233d5cdc9c2
Menzi Manqele <manqele@live.com>
2022-05-08 22:02:38 +0200

    MVC web app communication with Banking Microservice

---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TransferService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task Transfer(TransferDTO transferDTO)
+        {
+            var uri = "Https://localhost:5001/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDTO),System.Text.Encoding.UTF8, "application/json");    
+            var response = await _httpClient.PostAsync(uri, transferContent);   
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
